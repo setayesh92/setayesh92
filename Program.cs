acaaf9e653485d81647b9623e5da357f8e847f26
setayesh92 <36989055+setayesh92@users.noreply.github.com>
2018-05-11 15:37:15 +0430

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,158 +1,200 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Media;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace TicTacToeDemo
+namespace PhoneBookDemo
 {
     class Program
     {
-       
-            static void Main(string[] args)
+        static void Main(string[] args)
+        {
+            var names = new string[1];
+            var families = new string[1];
+            var phones = new string[1];
+            InitializePhoneBook();
+            while (true)
             {
-            
-
-            string player = "x";
-                string[,] board = new string[3, 3];
-                Initialize(board);
-                int current = 0;
-                
-                while (true)
+                Console.Clear();
+                var SelectedMenuItem = ShowMenu();
+                Console.Clear();
+                switch (SelectedMenuItem)
                 {
-
-                    Print(board);
-                    Console.WriteLine();
-                    Console.Write("Please enter row:");
-                    int row = int.Parse(Console.ReadLine());
-                    Console.Write("please enter col:");
-                    int col = int.Parse(Console.ReadLine());
-                    Console.Clear();
-                    board[row, col] = player;
-
-                    if (player == board[0, 0] && player == board[0, 1] && player == board[0, 2])
-                    {
-
-                        Print(board);
-                        Console.WriteLine(player + " won  game");
-                        Console.ReadKey();
+                    case 1:
+                        ShowContactList(names, families, phones);
+                        break;
+                    case 2:
+                        AddToContactList(ref names, ref families, ref phones);
                         break;
 
-                    }
-                    if (player == board[0, 0] && player == board[1, 0] && player == board[2, 0])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
+                    case 3:
+                        EditLFromContactList(ref names, ref families, ref phones);
+
                         break;
 
-                    }
-                    if (player == board[1, 0] && player == board[1, 1] && player == board[1, 2])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
+                    case 4:
+                        RemoveFromContactList(ref names, ref families, ref phones);
+
                         break;
-
-                    }
-                    if (player == board[2, 0] && player == board[2, 1] && player == board[2, 2])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
-                        break;
-
-                    }
-                    if (player == board[0, 0] && player == board[1, 1] && player == board[2, 2])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
-                        break;
-
-                    }
-                    if (player == board[2, 0] && player == board[1, 1] && player == board[0, 2])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
-                        break;
-
-                    }
-                    if (player == board[0, 1] && player == board[1, 1] && player == board[2, 1])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
-                        break;
-                    }
-                    if (player == board[0, 2] && player == board[1, 2] && player == board[2, 2])
-                    {
-                        Print(board);
-                        Console.WriteLine(player + "won  game");
-                        Console.ReadKey();
-                        break;
-
-                    }
-
-                current++;
-                    if (current == 9)
-                    {
-                        Print(board);
-                        Console.WriteLine("move");
-                        
-                    }
-                    player = changplayer(player);
-                }
-
-            }
-
-            static void Print(string[,] board)
-            {
-                Console.WriteLine(" 0  |1  |2  |");
-              
-            for (int row = 0; row < 3; row++)
-                {
-                    Console.Write(row + "|");
-                
-                for (int col = 0; col < 3; col++)
-                    {
-                        Console.Write(board[row, col]);
-                        Console.Write(" | ");
-                    
-                }
-                    Console.WriteLine();
-
 
                 }
             }
+        }
+        static void InitializePhoneBook()
+        {
+            Console.WriteLine("Welcome To PhoneBook");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+        static int ShowMenu()
+        {
+            Console.WriteLine($"1.ShowContactList\n2.AddNewContact\n3.EditContact\n4.RemoveContact\n5.Exit");
+            return int.Parse(Console.ReadLine());
+        }
 
-            static void Initialize(string[,] board)
+        static void ShowInvalidPhoneNotification()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Phone Number Is Not Valid!");
+            Console.CursorVisible = false;
+            Thread.Sleep(3000);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.CursorVisible = true;
+        }
+
+        static bool AddToContactList(ref String[] names, ref String[] families, ref string[] phones)
+        {
+            Console.Write("Name:");
+            var name = Console.ReadLine();
+            Console.Write("Family:");
+            var family = Console.ReadLine();
+            Console.Write("Phone:");
+            var phone = Console.ReadLine();
+            if (Regex.IsMatch(phone, @"^09\d{9}$"))
             {
-                for (int row = 0; row < 3; row++)
-                {
-                    for (int col = 0; col < 3; col++)
-                    {
-                        board[row, col] = " ";
+                AddToStringArray(ref names, name);
+                AddToStringArray(ref families, family);
+                AddToStringArray(ref phones, phone);
 
-                    }
-
-                }
+                return true;
+            }
+            else
+            {
+                return false;
 
             }
-            static string changplayer(string Cplayer)
+        }
+        static void AddToStringArray(ref String[] array, string newvalue)
+        {
+            string[] NewArray;
+            if (array[0] != null)
             {
-                if (Cplayer == "x")
+                NewArray = new string[array.Length + 1];
+            }
+            else
+            {
+                NewArray = new string[array.Length];
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                NewArray[i] = array[i];
+
+            }
+            NewArray[NewArray.Length - 1] = newvalue;
+            array = NewArray;
+
+        }
+
+        static void ShowContactList(string[] names, string[] families, string[] phones)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] != null)
                 {
-                    return "o";
-                }
-                else
-                {
-                    return "x";
+                    Console.WriteLine($"{names[i]}\t{families[i]}\t{phones[i]}\t");
                 }
             }
-       
+
+            Console.WriteLine("-------------------------------");
+            Console.ReadKey();
+
+        }
+
+        static bool RemoveFromContactList(ref string[] names, ref string[] families, ref string[] phones)
+        {
+
+
+            int i = 0;
+            for (; i < names.Length; i++)
+
+                RemoveFromArray(ref names, i);
+            RemoveFromArray(ref families, i);
+            RemoveFromArray(ref phones, i);
+            Console.Clear();
+            Console.WriteLine("list removed");
+            Thread.Sleep(3000);
+            return true;
+        }
+        static void RemoveFromArray(ref string[] array1, int oldvalue)
+        {
+
+
+            String[] OldArray = new string[array1.Length - 1];
+
+            int i = 0;
+            int j = 0;
+            while (i < OldArray.Length)
+            {
+                if (i != oldvalue)
+                {
+                    OldArray[j] = array1[i];
+                    j++;
+                }
+
+                i++;
+            }
+
+            array1 = OldArray;
+
+        }
+
+        static bool EditLFromContactList(ref string[] names, ref string[] families, ref string[] phones)
+        {
+              int j = 0;
+
+            ShowContactList(names, families,  phones);
+            for (; j < names.Length; j++)
+            {
+                EditArray(  ref names, j);
+                EditArray(  ref families, j);
+                EditArray(ref   phones, j);
+             }
+            return true;
+
+        }
+        static void EditArray(  ref string[] array2, int editvalue)
+        {
+            String[] OldArray = new string[array2.Length];
+            int i = 0;
+            for (; i < array2.Length; i++)
+            {
+                if (i == editvalue)
+                    array2[i] = Console.ReadLine();
+                OldArray[i] = array2[i];
+
+            }
+            array2 = OldArray;
+        }
+
 
     }
+
 }
+
