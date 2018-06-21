7d9097aa477ac312e219ed8b190c297f8ef9e520
setayesh92 <36989055+setayesh92@users.noreply.github.com>
2018-06-21 17:31:42 +0430

    Add files via upload

---
+++ AdminController.cs
@@ -0,0 +1,35 @@
+﻿using ModelShopOnline.Model;
+using ShopOnlineMvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnlineMvc1.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(DetailProductPainting p )
+        {
+            ShopOnlineDbContext ctx = new ShopOnlineDbContext();
+            ctx.Product.Add(p);
+            ctx.SaveChanges();
+            return Content("ثبت با موفقیت انجام شد ");
+           
+
+
+        }
+        public ActionResult ProductList()
+        {
+            return View();
+        } 
+
+    }
+}
